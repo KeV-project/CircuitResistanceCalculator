93a22c036af2fa79b2c96890133267cc0049b7ac
Kabanova Ekaterina <katovskaya009@gmail.com>
2020-12-03 12:09:35 +0700

    feature(CircuitResistanceCalculator): добавлен класс Elements

---
+++ CircuitResistanceCalculator/CircuitResistanceCalculator/Elements.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircuitResistanceCalculator
+{
+	public class Elements
+	{
+		private byte _resistorLastIndex = 0;
+		private byte _indectorLastIndex = 0;
+		private byte _capacitorLastIndex = 0;
+
+		private List<Element> _elements;
+
+		public Elements()
+		{
+			_elements = new List<Element>();
+		}
+
+		public void AddElement(Element element)
+		{
+			_elements.Add(element);
+		}
+	}
+}
