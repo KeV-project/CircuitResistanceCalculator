15bafecae0305408e1a1729f6a361671dfd6039e
Kabanova Ekaterina <katovskaya009@gmail.com>
2020-12-03 22:49:30 +0700

    docs(Capacitor): добавлены XML-комментарии

--- CircuitResistanceCalculator/CircuitResistanceCalculator/Capacitor.cs
+++ CircuitResistanceCalculator/CircuitResistanceCalculator/Capacitor.cs
@@ -7,12 +7,27 @@ using System.Numerics;
 
 namespace CircuitResistanceCalculator
 {
+	/// <summary>
+	/// Класс <see cref="Capacitor"> предназначен для 
+	/// представления емкостного элемента 
+	/// электрической цепи
+	/// </summary>
 	public class Capacitor : Element
 	{
+		/// <summary>
+		/// Инициализирует объект класса <see cref="Capacitor">
+		/// </summary>
+		/// <param name="value">Номинал элемента</param>
 		public Capacitor(double value) : base(value)
 		{
 
 		}
+
+		/// <summary>
+		/// Производит рассчет комплексного соротивления конденсатора
+		/// </summary>
+		/// <param name="frequency"> Частота сигнала</param>
+		/// <returns></returns>
 		public override Complex CalculateZ(double frequency)
 		{
 			return new Complex(0, -1) * 1 / 
