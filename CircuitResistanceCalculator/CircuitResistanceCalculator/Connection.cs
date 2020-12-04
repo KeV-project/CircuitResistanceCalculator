606e254db93ce58e9a2da4ae669f283ffc9104ec
Kabanova Ekaterina <katovskaya009@gmail.com>
2020-12-04 15:51:49 +0700

    doc(Connection): добавлены XML-комментарии

--- CircuitResistanceCalculator/CircuitResistanceCalculator/Connection.cs
+++ CircuitResistanceCalculator/CircuitResistanceCalculator/Connection.cs
@@ -6,30 +6,56 @@ using System.Threading.Tasks;
 
 namespace CircuitResistanceCalculator
 {
+	/// <summary>
+	/// Класс <see cref="Connection"> предоставляет общий 
+	/// функционал узелов дерева, определяющих тип соединения элементов
+	/// </summary>
 	public abstract class Connection : Node
 	{
+		/// <summary>
+		/// Сожержит список узлов подцепи, представляющих 
+		/// элементы или тип их соединения
+		/// </summary>
 		public List<Node> Connections { get; set; }
 
+		/// <summary>
+		/// Инициализирует общие свойства наследников 
+		/// класса <see cref="Connection">
+		/// </summary>
 		public Connection()
 		{
 			Connections = new List<Node>();
 		}
 
+		/// <summary>
+		/// Добавляет узел в подцепь
+		/// </summary>
+		/// <param name="node">Новый узел</param>
 		public void AddNode(Node node)
 		{
 			if(node is Element)
 			{
-				((Element)node).Index = Elements.GetIndex((Element)node);
+				Elements.SetIndex((Element)node);
 			}
 			node.ValueChanged += ChangeValue;
 			Connections.Add(node);
 		}
 
+		/// <summary>
+		/// Вызывавает событие перерасчета цепи при изменении
+		/// какого-либо элемента списка
+		/// </summary>
+		/// <param name="obj">Узел, вызвавший метод</param>
+		/// <param name="e">Прочие данные</param>
 		public void ChangeValue(object obj, EventArgs e)
 		{
 			ValueChanged?.Invoke(this, EventArgs.Empty);
 		}
 
+		/// <summary>
+		/// Событие, возникающее при изменении свойства 
+		/// <see cref="Value"> какого-либо элемента списка
+		/// </summary>
 		public override event EventHandler<EventArgs> ValueChanged;
 	}
 }
