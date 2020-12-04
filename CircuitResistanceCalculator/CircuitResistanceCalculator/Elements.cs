200e7a0907cf1ccf399224a6150c2128d305e411
Kabanova Ekaterina <katovskaya009@gmail.com>
2020-12-04 15:37:27 +0700

    docs(Elements): добавлены XML-комментарии

--- CircuitResistanceCalculator/CircuitResistanceCalculator/Elements.cs
+++ CircuitResistanceCalculator/CircuitResistanceCalculator/Elements.cs
@@ -6,19 +6,43 @@ using System.Threading.Tasks;
 
 namespace CircuitResistanceCalculator
 {
+	/// <summary>
+	/// Класс <see cref="Elements"> предназначен для назначения
+	/// индекса пассивным элементам электрической цепи
+	/// </summary>
 	public static class Elements
 	{
+		/// <summary>
+		/// Последний выданный индекс резистивному элементу
+		/// </summary>
 		private static byte _resistorLastIndex = 0;
+		/// <summary>
+		/// Последний выданный индекс индуктивному элементу
+		/// </summary>
 		private static byte _indectorLastIndex = 0;
+		/// <summary>
+		/// Последний выданный индекс емкостному элементу
+		/// </summary>
 		private static byte _capacitorLastIndex = 0;
 
+		/// <summary>
+		/// Список всех элементов электрической цепи
+		/// </summary>
 		private static List<Element> _elements;
 
+		/// <summary>
+		/// Инициализирует совйства класса <see cref="Elements">
+		/// </summary>
 		static Elements()
 		{
 			_elements = new List<Element>();
 		}
 
+		/// <summary>
+		/// Добавляет элемент в список класса <see cref="Elements">
+		/// и назначает ему актуальный индекс
+		/// </summary>
+		/// <param name="element">Новый элемент цепи</param>
 		public static void SetIndex(Element element)
 		{ 
 
