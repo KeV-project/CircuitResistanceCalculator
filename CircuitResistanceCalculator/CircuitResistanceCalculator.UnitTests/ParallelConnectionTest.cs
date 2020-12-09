c9edf8541548cbac72574303cf198e312fa653cd
Kabanova Ekaterina <katovskaya009@gmail.com>
2020-12-09 17:48:02 +0700

    feature(CircuitResistanceCalculator.UnitTests): добавлены тесты для класса ParallelConnection

---
+++ CircuitResistanceCalculator/CircuitResistanceCalculator.UnitTests/ParallelConnectionTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.Numerics;
+
+namespace CircuitResistanceCalculator.UnitTests
+{
+	[TestFixture]
+	class ParallelConnectionTest
+	{
+		[Test(Description = "Позитивный тест метода CalculateZ")]
+		public void TestCalculateZ_CorrectValue()
+		{
+			// arrenge
+			ParallelConnection parallelConnection = new ParallelConnection();
+			parallelConnection.AddNode(new Resistor(1000.0));
+			parallelConnection.AddNode(new Inductor(0.016));
+			parallelConnection.AddNode(new Capacitor(0.00022116));
+
+			double frequency = 50.0;
+			Complex expectedZ = new Complex(0.06, 7.716);
+
+			// act
+			Complex actualZ = parallelConnection.CalculateZ(frequency);
+
+			// assert
+			Assert.AreEqual(expectedZ, actualZ, "Метод неверно " +
+				"рассчитывает комплексное сопротивление " +
+				"последовательно цепи");
+
+		}
+	}
+}
