4c45fc901c0f4e873ebfc5b2cd5c61bf9e963a2c
Kabanova Ekaterina <katovskaya009@gmail.com>
2021-02-20 17:36:35 +0700

    docs(Drawers): добавлены xml-комментарии

--- CircuitResistanceCalculator/CircuitDrawer/Drawers/Drawer.cs
+++ CircuitResistanceCalculator/CircuitDrawer/Drawers/Drawer.cs
@@ -2,8 +2,21 @@
 
 namespace CircuitVisualization.Drawers
 {
+	/// <summary>
+	/// Класс <see cref="Drawer"/> предназначен отрисовки фигур на изображении
+	/// </summary>
 	public static class Drawer
 	{
+		/// <summary>
+		/// Рисует линию на изображении
+		/// </summary>
+		/// <param name="bitmap">Изображение</param>
+		/// <param name="x1">Начальная абцисса линии</param>
+		/// <param name="y1">Начальная ордината линии</param>
+		/// <param name="x2">Конечная абцисса линии</param>
+		/// <param name="y2">Конечная ордината линии</param>
+		/// <param name="lineColor">Цвет линии</param>
+		/// <param name="lineWidth">Ширина линии</param>
 		public static void DrawLine(Bitmap bitmap, int x1, int y1, 
 			int x2, int y2, Color lineColor, int lineWidth)
 		{
