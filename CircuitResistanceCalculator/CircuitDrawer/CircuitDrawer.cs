da567cce288349ea878223688cc80dd2094158c8
Kabanova Ekaterina <katovskaya009@gmail.com>
2021-02-20 17:38:21 +0700

    docs(CircuitDrawer): добавлены xml-комментарии

--- CircuitResistanceCalculator/CircuitDrawer/CircuitDrawer.cs
+++ CircuitResistanceCalculator/CircuitDrawer/CircuitDrawer.cs
@@ -4,8 +4,17 @@ using CircuitResistanceCalculator.Connection;
 
 namespace CircuitVisualization
 {
+    /// <summary>
+    /// Класс <see cref="CircuitDrawer"/> предназначен для
+    /// отрисовки электрической цепи
+    /// </summary>
     public static class CircuitDrawer
     {
+        /// <summary>
+        /// Отрисовывает электрическую цепь на изображении
+        /// </summary>
+        /// <param name="circuit">Макет электрической цепи</param>
+        /// <param name="bitmap">Изображения</param>
         public static void Draw(ConnectionBase circuit, 
             Bitmap bitmap)
 		{
